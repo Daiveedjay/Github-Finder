0b5d0e08ac2043638430c002ae2887d685b3cdf3
Daiveed <jajadavidjid@gmail.com>
2023-02-02 13:08:05 +0100

    Second commit

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use strict";
 
-// const userName = document.getElementsByClassName("user--name");
-// console.log(userName);
+// SELECT EVERY ELEMENT
 let userName = document.querySelector(".user--name");
 let userTag = document.querySelector(".user--tag");
 let userBio = document.querySelector(".user--bio");
@@ -14,82 +13,101 @@ let twitter = document.querySelector(".twitter--handle");
 let portfolio = document.querySelector(".portfolio--text");
 let github = document.querySelector(".github--handle");
 let avatar = document.querySelector(".avatar");
-console.log(avatar);
 let searchButton = document.querySelector(".search--button");
 let search = document.querySelector(".search");
 let body = document.querySelector("body");
 let socials = document.querySelector(".profile__socials");
 let loader = document.querySelector(".loader");
 let mainContainer = document.querySelector(".main__container");
-
-// let lightMode = document.querySelector(".light--mode");
-// let darkMode = document.querySelector(".dark--mode");
 let themeChanger = document.querySelector(".theme--switch");
 
+// TOGGLE THEMES
 themeChanger.addEventListener("click", function () {
   document.body.classList.toggle("light__mode");
   searchButton.classList.toggle("button-fix");
+  if (document.body.classList.contains("light__mode")) {
+    localStorage.setItem("themeMode", "light__mode");
+  } else {
+    localStorage.removeItem("themeMode");
+  }
 });
 
-// lightMode.addEventListener("click", function () {
-//   lightMode.src = `./Github-user-search-main/src/assets/icon-sun-active.svg`;
-//   darkMode.src = `./Github-user-search-main/src/assets/icon-moon.svg`;
+// Handle modes
+const mode = localStorage.getItem("themeMode");
+if (mode === "light__mode") {
+  document.body.classList.add(mode);
+}
 
-// });
-// darkMode.addEventListener("click", function () {
-//   console.log(darkMode);
-//   darkMode.src = `./Github-user-search-main/src/assets/icon-moon-active.svg`;
-//   lightMode.src = `./Github-user-search-main/src/assets/icon-sun.svg`;
-// });
+// UPDATES THE UI
+const updateUI = function (userData) {
+  userName.textContent = userData.name;
 
+  userTag.textContent = userData.login;
+
+  userBio.textContent = userData.bio
+    ? userData.bio
+    : "This user is a mystery, even to me";
+
+  repoCount.textContent = userData.public_repos;
+
+  followers.textContent = userData.followers;
+
+  userLocation.textContent = userData.location
+    ? userData.location
+    : "This user is off grid";
+
+  following.textContent = userData.following;
+
+  twitter.textContent = userData.twitter_username
+    ? userData.twitter_username
+    : "This user doesn't like the bird app";
+
+  portfolio.textContent = userData.blog
+    ? userData.blog
+    : "This user has no personal links";
+
+  github.textContent = userData.html_url;
+
+  avatar.src = userData.avatar_url;
+};
+
+// FETCH USER DATA
 const getUserAndRepoData = async function (username) {
   try {
     loader.style.display = "block";
     const userResponse = await fetch(
       `https://api.github.com/users/${username}`
     );
+    // IF USER INPUTS WRONG OR INVALID STRING
     if (!userResponse.ok) {
       throw new Error("You searched for a ghost. Try again");
     }
 
+    // CONVERTS FULFILLED PROOMISE TO JSON
     const userData = await userResponse.json();
-    userName.textContent = userData.name;
-    userTag.textContent = userData.login;
-    userBio.textContent =
-      userData.bio || userData.bio === ""
-        ? ""
-        : "This user is a mystery, even to me";
+    updateUI(userData);
 
-    repoCount.textContent = userData.public_repos;
-    followers.textContent = userData.followers;
-    userLocation.textContent = userData.location;
+    localStorage.setItem("userData", JSON.stringify(userData));
 
-    following.textContent = userData.following;
-
-    twitter.textContent =
-      userData.twitter_username || userData.twitter_username === ""
-        ? ""
-        : "This user doesn't like the bird app";
-
-    portfolio.textContent = userData.blog;
-    github.textContent = userData.html_url;
-    avatar.src = userData.avatar_url;
-
-    console.log(userData);
     loader.style.display = "none";
-    console.log(userData.bio, userData.name);
   } catch (error) {
     search.value = error.message;
     setTimeout(function () {
       search.value = "";
+      loader.style.display = "none";
     }, 2000);
-
-    loader.style.display = "none";
-
-    console.error(error);
   }
 };
 
+const loadStoredData = function () {
+  const storedData = localStorage.getItem("userData");
+  const userData = JSON.parse(storedData);
+  updateUI(userData);
+};
+
+window.addEventListener("load", loadStoredData);
+
+// EVENT LISTENER TO FETCH DATA
 searchButton.addEventListener("click", function () {
   const username = search.value.toLowerCase();
 
@@ -98,25 +116,38 @@ searchButton.addEventListener("click", function () {
 });
 
 let number = 0;
+// ARRAY CONTAINING ALL CLICK SVGS
 const stamps = [
-  "./IMG/icon-location.svg",
-  "./IMG/icon-twitter.svg",
-  "./IMG/icon-website.svg",
-  "./IMG/icon-company.svg",
+  "./Click-svgs/circles.svg",
+  "./Click-svgs/heart.svg",
+  "./Click-svgs/moon.svg",
+  "./Click-svgs/rainbow.svg",
+  "./Click-svgs/shooting-star.svg",
+  "./Click-svgs/waves.svg",
 ];
 
 const stampsTag = document.querySelector("div.stamps");
 
-let img;
 const addStamp = function (x, y) {
-  // <img src="circles.svg">
-  img = document.createElement("img");
+  const img = document.createElement("img");
   img.setAttribute("src", stamps[number]);
-  console.log(x, y);
+  const randID = crypto.randomUUID();
+  img.setAttribute("id", randID);
+
+  setTimeout(function () {
+    const prevImg = document.getElementById(randID);
+    stampsTag.removeChild(prevImg);
+  }, 3000);
+
   // remove half the window width
   img.style.left = `${x - window.innerWidth / 2}px`;
   img.style.top = `${y}px`;
   stampsTag.appendChild(img);
+
+  // add some audio
+  const audio = document.createElement("audio");
+  audio.setAttribute("src", "plop.mp3");
+  audio.play();
 
   number = number + 1;
   if (number > stamps.length - 1) {
@@ -125,11 +156,9 @@ const addStamp = function (x, y) {
 };
 
 body.addEventListener("click", function (event) {
-  if (event.target === mainContainer) return;
-
   addStamp(event.pageX, event.pageY);
 });
 
-setTimeout(function () {
-  img.style.display = "none";
-}, 1500);
+mainContainer.addEventListener("click", function (event) {
+  event.stopPropagation();
+});
